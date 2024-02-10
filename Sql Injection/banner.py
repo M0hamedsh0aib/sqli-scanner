778669398dbd97f2106aabb39827639ea7856680
M0hamedsh0aib <108838188+M0hamedsh0aib@users.noreply.github.com>
2024-02-10 13:38:25 +0530

    banner.py

--- Sql Injection/banner.py
+++ Sql Injection/banner.py
@@ -1,8 +1,13 @@
 import argparse
 
 def banner():
-    print("""                                                                      
-                                                                                                  V 1.0
+    # ANSI escape codes for color formatting
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RESET = '\033[0m'  # Reset to default color
+    
+    print(RED + """
  $$$$$$\  $$$$$$\ $$\      $$$$$$\        $$$$$$\  $$$$$$\  $$$$$$\ $$\   $$\$$\   $$\$$$$$$$$\$$$$$$$\  
 $$  __$$\$$  __$$\$$ |     \_$$  _|      $$  __$$\$$  __$$\$$  __$$\$$$\  $$ $$$\  $$ $$  _____$$  __$$\ 
 $$ /  \__$$ /  $$ $$ |       $$ |        $$ /  \__$$ /  \__$$ /  $$ $$$$\ $$ $$$$\ $$ $$ |     $$ |  $$ |
@@ -11,14 +16,13 @@ $$ /  \__$$ /  $$ $$ |       $$ |        $$ /  \__$$ /  \__$$ /  $$ $$$$\ $$ $$$
 $$\   $$ $$ $$\$$ $$ |       $$ |        $$\   $$ $$ |  $$\$$ |  $$ $$ |\$$$ $$ |\$$$ $$ |     $$ |  $$ |
 \$$$$$$  \$$$$$$ /$$$$$$$$\$$$$$$\       \$$$$$$  \$$$$$$  $$ |  $$ $$ | \$$ $$ | \$$ $$$$$$$$\$$ |  $$ |
  \______/ \___$$$\\________\______|       \______/ \______/\__|  \__\__|  \__\__|  \__\________\__|  \__|
-              \___|                                                                                      
-                                                                                                         
-                                                                                                          
-                                                                                                @ms-shoaib"""
-           )
+              \___|                                                                                      """ + RESET)
+    
     parser = argparse.ArgumentParser(description="Scans SQLi Vulnerability")
     parser.add_argument("-u", "--url", type=str, help="URL to fetch content from", required=True)
 
     args = parser.parse_args()
     return args.url
- 
+
+if __name__ == "__main__":
+    banner()
